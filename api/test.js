9c9ada0b2e07a41669059f89c4fda6d8e7e0dec9
Kevin Chu <kevin@19hours.net>
2024-09-18 23:53:08 +0800

    test

--- api/test.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  supportsResponseStreaming: true,
-};
-
-export default function handler(request: Request, response: Response) {
-  const { name = "World" } = request.query;
-  return response.send(`Hello ${name}!`);
-}
