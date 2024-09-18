345e3595b7688e3ebc806817896cf4a481162932
Kevin Chu <kevin@19hours.net>
2024-09-18 22:29:19 +0800

    test

---
+++ api/example.js
@@ -0,0 +1,21 @@
+export const config = {
+  runtime: "edge",
+};
+
+export async function GET() {
+  // This encoder will stream your text
+  const encoder = new TextEncoder();
+  const customReadable = new ReadableStream({
+    start(controller) {
+      // Start encoding 'Basic Streaming Test',
+      // and add the resulting stream to the queue
+      controller.enqueue(encoder.encode("Basic Streaming Test"));
+      // Prevent anything else being added to the stream
+      controller.close();
+    },
+  });
+
+  return new Response(customReadable, {
+    headers: { "Content-Type": "text/html; charset=utf-8" },
+  });
+}
