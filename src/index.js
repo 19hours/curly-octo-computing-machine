dec47dc4e891c874734ecc56aba25f271f82f77a
Kevin Chu <kevin@19hours.net>
2024-09-18 23:03:00 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { Readable } from "stream"; // Use Node.js Readable stream
-import { TextEncoder } from "util"; // Use TextEncoder from Node.js
 
 const app = express();
 const port = 4001;
@@ -11,31 +10,28 @@ app.get("/", (req, res) => {
   res.setHeader("Cache-Control", "no-cache, no-transform");
   res.setHeader("Connection", "keep-alive");
 
-  // Use TextEncoder to encode the streamed messages
-  const encoder = new TextEncoder();
-
   // Counter to control the number of messages sent
   let i = 0;
 
   // Create a custom Readable stream that controls when to push data
   const customReadable = new Readable({
-    async read() {
-      if (i === 0) {
-        // Send the initial message
-        this.push(encoder.encode("data: Basic Streaming Test\n\n"));
-        i++;
-      } else if (i <= 10) {
-        // Send a message every second
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-        const message = `data: ${i - 1}\n\n`; // Send 0 to 9 as the message
-        this.push(encoder.encode(message)); // Push the message to the stream
-        i++;
-      } else {
-        // End the stream after 10 messages
-        this.push(null); // Signal the end of the stream
-      }
-    },
+    read() {},
   });
+
+  // Send the initial message
+  customReadable.push("data: Basic Streaming Test\n\n");
+
+  // Send a message every second
+  const interval = setInterval(() => {
+    if (i < 10) {
+      customReadable.push(`data: ${i}\n\n`); // Send 0 to 9 as the message
+      i++;
+    } else {
+      // End the stream after 10 messages
+      customReadable.push(null); // Signal the end of the stream
+      clearInterval(interval);
+    }
+  }, 1000);
 
   // Pipe the readable stream into the response
   customReadable.pipe(res);
