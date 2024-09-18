fc0605bda9c566300919bf93fb5c99f4f4c57bcb
Kevin Chu <kevin@19hours.net>
2024-09-18 21:45:11 +0800

    test

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const port = 4001;
 
+export const dynamic = "force-dynamic"; // defaults to auto
+
 app.get("/", async (req, res) => {
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache, no-transform");
